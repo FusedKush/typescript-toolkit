39b048624e1742b8ea38934dd6e93223b1ac270b
Zach Vaughan <zach@zvaughan.com>
2025-05-16 13:32:27 -0600

    Add the `arrays` Namespace (#5)
    Add the `arrays` Namespace for utility tools used when working with Dynamic-Length Arrays.

---
+++ toolkit/arrays/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Utility tools for working with
+ * [Dynamic-Length Arrays](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Array).
+ * 
+ * @since 1.0.0
+ */
+declare module "./index.js";
+
+export * as default from "./index.js";
