cfdccef07c2e6d6a9df3ef6a39c267939d24e1b0
Zach Vaughan <zach@zvaughan.com>
2025-05-16 18:14:27 -0600

    Remove `toolkit/examples`
    Remove the `examples` Namespace and the
    `examples/exampleTool` Tool, both of which served
    no use beyond being temporary placeholders that are no longer needed.

--- toolkit/examples/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Example tools.
- * 
- * @since 0.0.1
- */
-declare module "./index.js";
-
-export * from "./exampleTool/index.js";
-export * as default from "./index.js";
--- toolkit/index.ts
+++ toolkit/index.ts
@@ -4,6 +4,5 @@
 declare module "./index.js";
 
 export * as arrays from "./arrays/index.js";
-export * as examples from "./examples/index.js";
 export * as types from "./types/index.js";
 export * as default from "./index.js";
